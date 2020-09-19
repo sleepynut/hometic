03a7a46beb88683fd9679ab615562489dde437ab
Nut Pawasuthiwong <nut.pa@kbtg.tech>
2020-09-19 16:27:41 +0700

    injection

--- server.go
+++ server.go
@@ -17,11 +17,48 @@ type Pair struct {
 	UserID   int
 }
 
+type PairDeviceHandler struct {
+	createPairDevice CreatePairDevice
+}
+
+func (ph *PairDeviceHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	var p Pair
+	err := json.NewDecoder(r.Body).Decode(&p)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(err.Error())
+		return
+	}
+	defer r.Body.Close()
+	fmt.Printf("pair: %#v\n", p)
+
+	err = ph.createPairDevice(p)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(err.Error())
+		return
+	}
+
+	w.Write([]byte(`{"status":"active"}`))
+}
+
+type CreatePairDevice func(p Pair) error
+
+func createPairDevice(p Pair) error {
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = db.Exec("INSERT INTO pairs VALUES (?,?);", p.DeviceID, p.UserID)
+	return err
+}
+
 func main() {
-	fmt.Println("hello hometic : I'm Gpher!!")
+	fmt.Println("hello hometic : I'm Gopher!!")
 
 	r := mux.NewRouter()
-	r.HandleFunc("/pair-device", PairDeviceHandler).Methods(http.MethodPost)
+	r.Handle("/pair-device", &PairDeviceHandler{createPairDevice}).Methods(http.MethodPost)
 
 	addr := fmt.Sprintf("0.0.0.0:%s", os.Getenv("PORT"))
 	fmt.Println("addr:", addr)
@@ -31,37 +68,37 @@ func main() {
 		Handler: r,
 	}
 
-	log.Println("...starting...")
+	log.Println("starting...")
 	log.Fatal(server.ListenAndServe())
 }
 
-func PairDeviceHandler(w http.ResponseWriter, r *http.Request) {
-	var p Pair
-	err := json.NewDecoder(r.Body).Decode(&p)
+// func PairDeviceHandler(w http.ResponseWriter, r *http.Request) {
+// 	var p Pair
+// 	err := json.NewDecoder(r.Body).Decode(&p)
 
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(w).Encode(err.Error())
-		return
-	}
+// 	if err != nil {
+// 		w.WriteHeader(http.StatusBadRequest)
+// 		json.NewEncoder(w).Encode(err.Error())
+// 		return
+// 	}
 
-	defer r.Body.Close()
-	fmt.Printf("pair: %#v\n", p)
+// 	defer r.Body.Close()
+// 	fmt.Printf("pair: %#v\n", p)
 
-	// open database connection
-	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
-	if err != nil {
-		log.Fatal("connect to database error", err)
-	}
-	defer db.Close()
+// 	// open database connection
+// 	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+// 	if err != nil {
+// 		log.Fatal("connect to database error", err)
+// 	}
+// 	defer db.Close()
 
-	// insert p into table pairs
-	_, err = db.Exec("INSERT INTO pairs VALUES ($1,$2);", p.DeviceID, p.UserID)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode(err.Error())
-		return
-	}
+// 	// insert p into table pairs
+// 	_, err = db.Exec("INSERT INTO pairs VALUES ($1,$2);", p.DeviceID, p.UserID)
+// 	if err != nil {
+// 		w.WriteHeader(http.StatusInternalServerError)
+// 		json.NewEncoder(w).Encode(err.Error())
+// 		return
+// 	}
 
-	w.Write([]byte(`{"status":"active"}`))
-}
+// 	w.Write([]byte(`{"status":"active"}`))
+// }
--- server_test.go
+++ server_test.go
@@ -1,3 +1,32 @@
+// package main
+
+// import (
+// 	"bytes"
+// 	"encoding/json"
+// 	"net/http"
+// 	"net/http/httptest"
+// 	"testing"
+// )
+
+// func TestCreatePairDevice(t *testing.T) {
+// 	payload := new(bytes.Buffer)
+// 	json.NewEncoder(payload).Encode(Pair{DeviceID: 1234, UserID: 4433})
+
+// 	req := httptest.NewRequest(http.MethodPost, "/pair-device", payload)
+// 	rec := httptest.NewRecorder()
+
+// 	PairDeviceHandler(rec, req)
+
+// 	if http.StatusOK != rec.Code {
+// 		t.Error("expect 200 but got ", rec.Code)
+// 	}
+
+// 	expected := `{"status":"active"}`
+// 	if rec.Body.String() != expected {
+// 		t.Errorf("expected %q but got %q\n", expected, rec.Body.String())
+// 	}
+// }
+
 package main
 
 import (
@@ -11,14 +40,17 @@ import (
 func TestCreatePairDevice(t *testing.T) {
 	payload := new(bytes.Buffer)
 	json.NewEncoder(payload).Encode(Pair{DeviceID: 1234, UserID: 4433})
-
 	req := httptest.NewRequest(http.MethodPost, "/pair-device", payload)
 	rec := httptest.NewRecorder()
 
-	PairDeviceHandler(rec, req)
+	handler := &PairDeviceHandler{createPairDevice: func(p Pair) error {
+		return nil
+	}}
+
+	handler.ServeHTTP(rec, req)
 
 	if http.StatusOK != rec.Code {
-		t.Error("expect 200 but got ", rec.Code)
+		t.Error("expect 200 OK but got ", rec.Code)
 	}
 
 	expected := `{"status":"active"}`
