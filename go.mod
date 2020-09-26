5f5bd65196579528aead36d036fe49c42b2fdad3
Nut Pawasuthiwong <nut.pa@kbtg.tech>
2020-09-26 11:43:17 +0700

    add all content-type

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/sleepynut/hometic
 go 1.15
 
 require (
+	github.com/anuchito/hometic v0.0.0-20200918153409-106754fe0ef9
 	github.com/gorilla/mux v1.8.0
 	github.com/lib/pq v1.8.0
 	go.uber.org/zap v1.16.0
--- server.go
+++ server.go
@@ -70,11 +70,13 @@ func PairDeviceHandler(device Device) http.HandlerFunc {
 
 		err = device.Pair(p)
 		if err != nil {
+			w.Header().Set("content-type", "application/json")
 			w.WriteHeader(http.StatusInternalServerError)
 			json.NewEncoder(w).Encode(err.Error())
 			return
 		}
 
+		w.Header().Set("content-type", "application/json")
 		w.Write([]byte(`{"status":"active"}`))
 	}
 }
